3f4de55e9fb80e7e64b742791d3f4286a945b975
Sergio Sobrado <l20211848@tectijuana.edu.mx>
2021-03-09 12:39:50 -0800

    Primer commit
    :+1: :a: mi primer commit

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Usuario
+{
+    class Usuario 
+    {
+        // Atributos Campos 
+        private string Nombre;
+        private string Correo;
+        private int Seguidores;
+
+        // 
+        public void Publicar(string Texto)
+        {
+            Console.WriteLine($"{Nombre}:  Acaba de publicar lo siguiente:  {Texto} ");  
+        }
+        public void VerPerfil()
+        {
+            Console.WriteLine($"User: {Nombre}, Correo: {Correo}, Seguidores: {Seguidores}");
+        }
+        public void SetName(string nombre)
+        {
+            // TO DO: Verificar que el nombre de usuario no existe 
+            this.Nombre = nombre;
+        }
+        public void SetCorrep(string correo)
+        {
+            // TO DO: Verificar que el nombre de usuario no existe 
+            this.Correo = correo;
+        }
+        public void SetSegudores(int seguidores)
+        {
+            // TO DO: Verificar que el nombre de usuario no existe 
+            if (seguidores < 0)
+            
+                seguidores = seguidores  * -1;
+                Seguidores = seguidores;
+            
+           
+
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario Mario = new Usuario();
+
+            // Mario.SetName = "Mariosky";
+            Mario.SetName("Mariosky");
+            Mario.SetCorrep("Mariosky@gmail.com");
+            Mario.SetSegudores(80);
+
+
+            Mario.VerPerfil();
+            Mario.Publicar("Hola a todos:3");
+
+            Console.ReadKey();
+        }
+    }
+}
